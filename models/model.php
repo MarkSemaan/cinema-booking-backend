4d4063d06f43e1063424159c5aec0eb394cd455d
MarkSemaan <mark.h.semaan@gmail.com>
2025-06-28 14:49:35 +0300

    added update, findby and get_param_Types functions to base model

--- models/model.php
+++ models/model.php
@@ -43,6 +43,7 @@ abstract class Model
         $values = array_values($filteredData);
         $types = str_repeat('s', count($values));
 
+        $types = $this->get_param_types($values);
         $stmt->bind_param($types, ...$values);
 
         if ($stmt->execute()) {
@@ -50,6 +51,68 @@ abstract class Model
         } else {
             throw new Exception("Error creating record: " . $stmt->error);
         }
+    }
+
+    public function update(int $id, array $data)
+    {
+        $filteredData = array_filter(
+            $data,
+            fn($key) => in_array($key, $this->fillable),
+            ARRAY_FILTER_USE_KEY
+        );
+
+        $setClauses = [];
+        $values = [];
+        foreach ($filteredData as $column => $value) {
+            $setClauses[] = "{$column} = ?";
+            $values[] = $value;
+        }
+
+        if (empty($setClauses)) {
+            return false; // No data to update
+        }
+
+        $setClause = implode(", ", $setClauses);
+        $sql = "UPDATE {$this->table} SET {$setClause} WHERE id = ?";
+        $stmt = $this->mysqli->prepare($sql);
+
+        $values[] = $id; // Add ID to the end for binding
+        $types = $this->get_param_types($values);
+
+        $stmt->bind_param($types, ...$values);
+
+        if ($stmt->execute()) {
+            return $stmt->affected_rows > 0;
+        } else {
+            throw new Exception("Error updating record: " . $stmt->error);
+        }
+    }
+
+    public function findBy(string $column, $value)
+    {
+        $stmt = $this->mysqli->prepare("SELECT * FROM {$this->table} WHERE {$column} = ?");
+        $types = $this->get_param_types([$value]);
+        $stmt->bind_param($types, $value);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    protected function get_param_types(array $values): string
+    {
+        $types = '';
+        foreach ($values as $value) {
+            if (is_int($value)) {
+                $types .= 'i';
+            } elseif (is_float($value)) {
+                $types .= 'd';
+            } elseif (is_string($value)) {
+                $types .= 's';
+            } else {
+                $types .= 'b'; // Blob for other types
+            }
+        }
+        return $types;
     }
     public function delete(int $id)
     {
