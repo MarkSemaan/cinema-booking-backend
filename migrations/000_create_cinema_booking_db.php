8971105bff69f882b202a6fd620f34b8d8b05542
MarkSemaan <mark.h.semaan@gmail.com>
2025-06-28 16:45:00 +0300

    modified create_cinema_booking db migration to drop and re-create the database

--- migrations/000_create_cinema_booking_db.php
+++ migrations/000_create_cinema_booking_db.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../connection/db_connection.php';
 
 function createDB($mysqli)
 {
+    $mysqli->query("DROP DATABASE IF EXISTS cinema_booking_db");
     $sql = "CREATE DATABASE IF NOT EXISTS cinema_booking_db";
     if ($mysqli->query($sql) === TRUE) {
         echo "Database created successfully\n";
