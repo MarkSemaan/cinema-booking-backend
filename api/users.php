1d7287e34ef5d2a68dfb870863743de946a8eaa7
MarkSemaan <mark.h.semaan@gmail.com>
2025-06-29 18:58:57 +0300

    Add CORS support to API endpoints to get axios working

--- api/users.php
+++ api/users.php
@@ -2,7 +2,17 @@
 
 require_once __DIR__ . '/../controllers/UsersController.php';
 
+// Add CORS headers to allow cross-origin requests
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $controller = new UsersController();
 $action = $_GET['action'] ?? null; //Get the action from the request
