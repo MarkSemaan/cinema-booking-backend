a37b5b4d482b1333f3be3356e2916c46bed7a9bf
MarkSemaan <mark.h.semaan@gmail.com>
2025-06-29 22:59:43 +0300

    created 3 more movies for the movieseeder

--- seeders/MovieSeeder.php
+++ seeders/MovieSeeder.php
@@ -21,7 +21,7 @@ class MovieSeeder extends Seeder
             'title' => 'Avatar: The Way of Water',
             'director' => 'James Cameron',
             'release_year' => 2022,
-            'genre' => 'Action',
+            'genre' => 'Adventure',
             'poster_url' => '/assets/movie_poster_2.jpg',
             'synopsis' => 'Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na\'vi race to protect their home.'
         ]);
@@ -29,10 +29,34 @@ class MovieSeeder extends Seeder
             'title' => 'Spider-Man: No Way Home',
             'director' => 'Jon Watts',
             'release_year' => 2021,
-            'genre' => 'Action',
+            'genre' => 'Superhero',
             'poster_url' => '/assets/movie_poster_3.jpg',
             'synopsis' => 'With Spider-Man\'s identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.'
         ]);
-        echo "Created Movies with IDs: $movie1_id, $movie2_id, $movie3_id\n\n";
+        $movie4_id = $movieModel->create([
+            'title' => 'The Dark Knight',
+            'director' => 'Christopher Nolan',
+            'release_year' => 2008,
+            'genre' => 'Superhero',
+            'poster_url' => '/assets/movie_poster_4.jpg',
+            'synopsis' => 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.'
+        ]);
+        $movie5_id = $movieModel->create([
+            'title' => 'Final Destination Bloodlines',
+            'director' => 'Zach Lipovsky',
+            'release_year' => 2025,
+            'genre' => 'Horror',
+            'poster_url' => '/assets/movie_poster_5.jpg',
+            'synopsis' => 'Plagued by a violent recurring nightmare, college student Stefanie heads home to track down the one person who might be able to break the cycle and save her family from the grisly demise that inevitably awaits them all.'
+        ]);
+        $movie6_id = $movieModel->create([
+            'title' => 'Fullstack Bootcamp',
+            'director' => 'SE Factory',
+            'release_year' => 2025,
+            'genre' => 'Horror',
+            'poster_url' => '/assets/movie_poster_6.jpg',
+            'synopsis' => 'A brutal and  unforgiving bootcamp that will test your limits and push you to your breaking point, can you survive termination?'
+        ]);
+        echo "Created Movies with IDs: $movie1_id, $movie2_id, $movie3_id, $movie4_id, $movie5_id, $movie6_id\n\n";
     }
 }
