aa800b01830eac66646665a7bb7c7d68de62f5a6
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-01 21:40:47 +0300

    Updated MovieSeeder to include genre because I missed it, oops?

--- seeders/MovieSeeder.php
+++ seeders/MovieSeeder.php
@@ -13,6 +13,7 @@ class MovieSeeder extends Seeder
             'title' => 'Top Gun: Maverick',
             'director' => 'Joseph Kosinski',
             'release_year' => 2022,
+            'genre' => 'Action',
             'poster_url' => '/assets/movie_poster_1.jpg',
             'synopsis' => 'After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN\'s elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.'
         ]);
@@ -20,6 +21,7 @@ class MovieSeeder extends Seeder
             'title' => 'Avatar: The Way of Water',
             'director' => 'James Cameron',
             'release_year' => 2022,
+            'genre' => 'Adventure',
             'poster_url' => '/assets/movie_poster_2.jpg',
             'synopsis' => 'Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na\'vi race to protect their home.'
         ]);
@@ -27,6 +29,7 @@ class MovieSeeder extends Seeder
             'title' => 'Spider-Man: No Way Home',
             'director' => 'Jon Watts',
             'release_year' => 2021,
+            'genre' => 'Superhero',
             'poster_url' => '/assets/movie_poster_3.jpg',
             'synopsis' => 'With Spider-Man\'s identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.'
         ]);
